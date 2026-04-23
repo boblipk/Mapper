5be9fb100cbddcde2240fe19e3db89e43d72193f
Cameron Murray <boblipk72@gmail.com>
2026-04-23 16:39:42 +0100

    devfine tile variables

--- Tile.java
+++ Tile.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+
 public class Tile {
+    private boolean upwall;
+    private boolean downwall;
+    private boolean leftwall;
+    private boolean rightwall;
+    private int x;
+    private int y;
+    private ArrayList<String> tags;
+    private String description;
     
 }
