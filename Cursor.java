c39ab7661ffd126d11b0b09b2a9d5f1c6b93a476
Cameron Murray <boblipk72@gmail.com>
2026-04-23 17:23:02 +0100

    Implement Cursor class with movement methods and initial position

--- Cursor.java
+++ Cursor.java
@@ -1,3 +1,25 @@
 public class Cursor {
-    
+    private int x;
+    private int y;
+
+    public Cursor() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public void moveUp() {
+        this.y -= 1;
+    }
+
+    public void moveDown() {
+        this.y += 1;
+    }
+
+    public void moveLeft() {
+        this.x -= 1;
+    }
+
+    public void moveRight() {
+        this.x += 1;
+    }
 }
