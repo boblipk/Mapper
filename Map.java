4a5dc90ae9dd39645cf98b2307a35014ba44b7a0
Cameron Murray <boblipk72@gmail.com>
2026-04-23 17:10:05 +0100

    define map constructor

--- Map.java
+++ Map.java
@@ -4,4 +4,12 @@ public class Map {
     private int maxX;
     private int minY;
     private int maxY;
+
+    public Map() {
+        this.tiles = new Tile[0][0];
+        this.minX = 0;
+        this.maxX = 0;
+        this.minY = 0;
+        this.maxY = 0;
+    }
 }
